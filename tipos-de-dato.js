ae801bfc765c4ad378b15d88de596b298cbc7d9b
danielgp <danielgarduno282@gmail.com>
2025-02-13 00:11:24 -0600

    Se agregaron ejemplos de tipos de datos

---
+++ tipos-de-dato.js
@@ -0,0 +1,33 @@
+//este es un comentario en javascript
+//console.log es una funcion que se utiliza para imprimir en consola
+console.log("Hola mundo");
+
+var nombre="Ana"; // tipo string
+var edad=25; // tipo number
+var sueldo=1.23; //number
+var casado= true; //boolean
+var hijos= null; // null
+var dato;//undefined 
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casado));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+
+/* 
+Comentario de varias lineas
+*/
+//ejercicios de campus
+console.log(typeof(42));
+console.log(typeof("veinticinco"));
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof(0));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(FALSE));
+console.log(typeof(false));
+console.log(typeof(True));
+console.log(typeof(1.234));
