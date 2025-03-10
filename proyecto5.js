a344c4ec40b7b2ac0c4c8df3560724bd66c3563f
danielgp <danielgarduno282@gmail.com>
2025-03-09 22:31:50 -0600

    Se creo el metodo para describir libros

--- proyecto5.js
+++ proyecto5.js
@@ -4,3 +4,8 @@ function libros(titulo, autor, anio, estado){
     this.anio=anio;
     this.estado=estado;
 }
+
+this.caracteristicaslibro=function(){
+    console.log(`Libro titulado ${this.titulo}, escrito por ${this.autor} en el año ${this.anio}, el estado es: ${this.estado}.`);
+    
+};
