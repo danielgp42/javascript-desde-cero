faf4353b8909ae12c6825399f413ef131d9a588a
danielgp <danielgarduno282@gmail.com>
2025-03-09 22:40:49 -0600

    Se agrego ejemplos y se llamó a la funcion

--- proyecto5.js
+++ proyecto5.js
@@ -3,9 +3,17 @@ function libros(titulo, autor, anio, estado){
     this.autor=autor;
     this.anio=anio;
     this.estado=estado;
-}
-
-this.caracteristicaslibro=function(){
-    console.log(`Libro titulado ${this.titulo}, escrito por ${this.autor} en el año ${this.anio}, el estado es: ${this.estado}.`);
-    
+    this.caracteristicaslibro=function(){
+        console.log(`Libro titulado ${this.titulo}, escrito por ${this.autor} en el año ${this.anio}, el estado del libro es: ${this.estado}.`);
+    }
 };
+
+//Ejemplos para probar el codigo
+
+let libro1 = new libros("El gallo de oro", "Juan Rulfo", 1980, "Prestado");
+let libro2 = new libros("Fuego y sangre", "George R. R. Martin", 2018, "Disponible");
+
+//llamar a la funcion para mostrar info de los libros
+
+libro1.caracteristicaslibro();
+libro2.caracteristicaslibro();
